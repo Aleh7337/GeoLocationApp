40bde5c3c57f069bc041d98e9f1b76239cab5461
Aleh7337 <ala.ara2005@gmail.com>
2024-05-28 10:19:40 +0200

    Added server

---
+++ TCPServ/TCPServ/ClientDescriptorClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections.Generic;
+namespace TCPServ {
+    enum StatoClient {
+        Attivo,
+        Sospeso,
+        Chiuso
+    }
+   class ClientDescriptorClass {
+        public Socket ClientSocketDescriptor=null;
+        public Thread ClientFormThreadDescriptor=null;
+        public Thread ClientRicezioneThread = null;
+        public StatoClient StatoC;
+        public IPAddress ClientIndirizzoIP;
+        public int ClientPorta;
+        public FormRiceviClient puntaForm;
+        public int ClientID = -1;
+        public String nomeClient = "";
+        public bool nuovoArrivo = true;
+        public String percorsoFile = "";
+        
+        public ClientDescriptorClass(Socket MioSocket,StatoClient MioStato,FormRiceviClient MiopuntaForm)
+        {
+            this.StatoC = MioStato; this.ClientSocketDescriptor = MioSocket;
+            this.puntaForm = MiopuntaForm;
+            this.ClientIndirizzoIP = ((IPEndPoint)(MioSocket.RemoteEndPoint)).Address;
+            ClientPorta = ((IPEndPoint)MioSocket.RemoteEndPoint).Port;
+        }
+   }
+}
---
+++ TCPServ/TCPServ/FormRiceviClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServ
+{
+    public partial class FormRiceviClient : Form
+    {
+        Socket SockClientCorrente;
+        public FormRiceviClient()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+        public FormRiceviClient(Socket sClient)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            SockClientCorrente = sClient;
+            InitializeComponent();
+            StatusBarClient.Items.Add("IP: " + ((IPEndPoint)(SockClientCorrente.RemoteEndPoint)).Address); //Fatto per riconoscere il client che si connette
+            StatusBarClient.Items.Add(" Porta: " + ((IPEndPoint)(SockClientCorrente.RemoteEndPoint)).Port);
+        }
+
+        public void ShowHideForm( bool show)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action< bool>(( isShow) => {
+                    if (isShow)
+                        this.Show();
+                    else
+                        this.Hide();
+                }), show);
+            }
+            else
+            {
+                if (show)
+                    this.Show();
+                else
+                    this.Hide();
+            } //if
+        } //ShowHideForm
+
+        public void FormRiceviClient_Load(object sender, EventArgs e)
+        {
+            //ImageBox.Image = Image.FromFile("download.jpg");
+        }
+    }
+}
